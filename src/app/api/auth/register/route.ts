1f9c84f6acc6565221c54a19764d88346a70574a
codiment <uprovisional51@gmail.com>
2025-04-03 23:57:30 -0300

    fix db

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -4,7 +4,7 @@ import {prisma} from "@/libs/prisma";
 export async function POST(request : Request) {
     const data = await request.json()
     console.log(data)
-
+ 
     const newUser = await prisma.user.create({
         data
     })
