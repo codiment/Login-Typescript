f6019c807797e185cfbcf69a970c163f76540eb6
codiment <uprovisional51@gmail.com>
2025-04-08 01:02:43 -0300

    commit 12

---
+++ src/app/dashboard/tasks/new/page.tsx
@@ -0,0 +1,13 @@
+import { Container, Heading } from "@radix-ui/themes"
+
+const TaskNewPage = () => {
+  return (
+    <div className="mt-10">
+      <Container>
+        <Heading>Task new page</Heading>
+      </Container>
+    </div>
+  )
+}
+
+export default TaskNewPage
